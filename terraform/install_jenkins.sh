64e64e5ca97f7d9d88c4c0e6d7a8096ff3617f75
Emmanue ELOKA <85324899+EmmanuelELOKA@users.noreply.github.com>
2022-01-17 17:49:59 -0500

    updated jenkins link

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -21,8 +21,8 @@ yum install docker -y
 sudo chkconfig docker on
 
 echo "Install Jenkins"
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install -y jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
