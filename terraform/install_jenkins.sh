cd0a2fe0129ea15c83a13389411ddfeb529d32b2
Emmanue ELOKA <85324899+EmmanuelELOKA@users.noreply.github.com>
2022-01-17 18:55:25 -0500

    recent update

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -4,28 +4,28 @@
 sudo yum -y update
 
 echo "Install Java JDK 8"
-yum remove -y java
-yum install -y java-1.8.0-openjdk
+sudo yum remove -y java
+sudo yum install -y java-1.8.0-openjdk
 
 echo "Install Maven"
-yum install -y maven 
+sudo yum install -y maven 
 
 echo "Install git"
-yum install -y git
+sudo yum install -y git
 
 echo "Install Docker engine"
-yum update -y
-yum install docker -y
-#sudo usermod -a -G docker jenkins
-#sudo service docker start
+sudo yum update -y
+sudo yum install docker -y
+sudo usermod -a -G docker jenkins
+sudo service docker start
 sudo chkconfig docker on
 
 echo "Install Jenkins"
-yum -y install wget
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo yum -y install wget
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install -y epel-release
-yum install -y jenkins
+sudo amazon-linux-extras install epel -y
+sudo yum install -y jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
